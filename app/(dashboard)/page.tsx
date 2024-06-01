a67b58120615e3e0ec0ce347b4d2b3f8ce3cdc0a
Malitha Sandaruwan <sandaruwanmalitha2018@gmail.com>
2024-06-01 21:36:39 +0530

    feat: Add form to  add new account

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+
 export default function DashboardPage() {
-  return <div></div>;
+  const { onOpen } = useNewAccount();
+  return (
+    <>
+      <Button type="button" onClick={onOpen}>
+        Add an account
+      </Button>
+    </>
+  );
 }
