99d52a2bb0f10faa8db97e7c6237821b8c54bce0
Malitha Sandaruwan <sandaruwanmalitha2018@gmail.com>
2024-06-01 19:36:33 +0530

    feat: Add GET endpoint to Accounts
    - Additional packages like tanstack query, configuring query provider etc.

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,26 +1,22 @@
 import { Hono } from "hono";
-import { z } from "zod";
-import { zValidator } from "@hono/zod-validator";
 import { handle } from "hono/vercel";
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 
+import accounts from "./accounts";
+import { HTTPException } from "hono/http-exception";
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", clerkMiddleware(), (c) => {
-  const auth = getAuth(c);
+app.onError((err, c) => {
+  if (err instanceof HTTPException)
+    return err.getResponse();
 
-  if (!auth?.userId) {
-    return c.json({
-      error: "Not authenticated",
-    });
-  }
-
-  return c.json({
-    message: "Hello Next.js!",
-  });
+  return c.json({ error: "Internal Server Error" }, 500);
 });
+
+const routes = app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
