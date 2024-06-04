c44334f7bb18027f43a08c286f4875059ffb310a
Malitha Sandaruwan <sandaruwanmalitha2018@gmail.com>
2024-06-04 21:27:35 +0530

    feat: Add react-papaparse dependency and implement CSV upload functionality

--- app/(dashboard)/transactions/category-column.tsx
+++ app/(dashboard)/transactions/category-column.tsx
@@ -1,4 +1,5 @@
 import { useOpenCategory } from "@/features/categories/hooks/use-open-category";
+import { useOpenTransaction } from "@/features/transactions/hooks/use-open-transaction";
 import { cn } from "@/lib/utils";
 import { TriangleAlert } from "lucide-react";
 
@@ -13,9 +14,12 @@ export const CategoryColumn = ({
   categoryId,
 }: Props) => {
   const { onOpen: onOpenCategory } = useOpenCategory();
+  const { onOpen: onOpenTransaction } =
+    useOpenTransaction();
 
   const onClick = () => {
     if (categoryId) onOpenCategory(categoryId);
+    else onOpenTransaction(id);
   };
 
   return (
--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -16,8 +16,39 @@ import { columns } from "@/app/(dashboard)/transactions/columns";
 import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction";
 import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
 import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
+import { useState } from "react";
+import { UploadButton } from "./upload-button";
+import { ImportCard } from "./import-card";
+
+enum VARIANTS {
+  LIST = "LIST",
+  IMPORT = "IMPORT",
+}
+
+const INITIAL_IMPORT_RESULTS = {
+  data: [],
+  errors: [],
+  meta: {},
+};
 
 export default function TransactionsPage() {
+  const [variant, setVariant] = useState(VARIANTS.LIST);
+  const [importResults, setImportResults] = useState(
+    INITIAL_IMPORT_RESULTS
+  );
+
+  const onUpload = (
+    results: typeof INITIAL_IMPORT_RESULTS
+  ) => {
+    setImportResults(results);
+    setVariant(VARIANTS.IMPORT);
+  };
+
+  const onCancelImport = () => {
+    setImportResults(INITIAL_IMPORT_RESULTS);
+    setVariant(VARIANTS.LIST);
+  };
+
   const newTransaction = useNewTransaction();
   const deleteTransaction = useBulkDeleteTransactions();
   const {
@@ -43,6 +74,19 @@ export default function TransactionsPage() {
       </div>
     );
   }
+
+  if (variant === VARIANTS.IMPORT) {
+    return (
+      <>
+        <ImportCard
+          data={importResults.data}
+          onCancel={onCancelImport}
+          onSubmit={() => {}}
+        />
+      </>
+    );
+  }
+
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <Card className="border-none drop-shadow-sm">
@@ -50,16 +94,23 @@ export default function TransactionsPage() {
           <CardTitle className="text-xl line-clamp-1">
             Transactions
           </CardTitle>
-          <Button size="sm" onClick={newTransaction.onOpen}>
-            <Plus className="size-4 mr-2" />
-            Add new
-          </Button>
+          <div className="flex flex-col lg:flex-row items-center gap-2">
+            <Button
+              size="sm"
+              className="w-full lg:w-auto"
+              onClick={newTransaction.onOpen}
+            >
+              <Plus className="size-4 mr-2" />
+              Add new
+            </Button>
+            <UploadButton onUpload={onUpload} />
+          </div>
         </CardHeader>
         <CardContent>
           <DataTable
             columns={columns}
             data={transactions || []}
-            filterKey={"amount"}
+            filterKey={"payee"}
             onDelete={(row) => {
               const ids = row.map((r) => r.original.id);
               deleteTransaction.mutate({ ids });
