16600b048aca7a87376a2174ef948902ddbfe376
Malitha Sandaruwan <sandaruwanmalitha2018@gmail.com>
2024-06-05 21:50:29 +0530

    feat: Add summary route to API

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import { eachDayOfInterval, isSameDay } from "date-fns";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -26,4 +27,52 @@ export function formatCurrency(
     currency: "USD",
     minimumFractionDigits: 2,
   }).format(modifiedValue);
+}
+
+export function calculatePercentageChange(
+  current: number,
+  previous: number
+) {
+  if (previous === 0) {
+    return previous === current ? 0 : 100;
+  }
+
+  return ((current - previous) / previous) * 100;
+}
+
+export function fillMissingDays(
+  activeDays: {
+    date: Date;
+    income: number;
+    expenses: number;
+  }[],
+  startDate: Date,
+  endDate: Date
+) {
+  if (activeDays.length === 0) {
+    return [];
+  }
+
+  const allDays = eachDayOfInterval({
+    start: startDate,
+    end: endDate,
+  });
+
+  const transactionsByDate = allDays.map((date) => {
+    const found = activeDays.find((day) =>
+      isSameDay(day.date, date)
+    );
+
+    if (found) {
+      return found;
+    } else {
+      return {
+        date,
+        income: 0,
+        expenses: 0,
+      };
+    }
+  });
+
+  return transactionsByDate;
 }
